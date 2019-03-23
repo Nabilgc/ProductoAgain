d6d1320fe8e2b9e1fe225e004a2778da0866456a
Nabil Galeb <nabilgaleb@hotmail.com>
2019-03-23 11:25:49 -0400

    API Mas azure

---
+++ ProyectoBackEnd/API2/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace API2.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        [Key]
+        public int ProductoID { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre del Producto")]
+
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+    }
+}
